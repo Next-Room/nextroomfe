33962629c7f1b3c7ebdb2ff15126e51ccc7909e3
anyl92 <yulim9592@gmail.com>
2026-07-19 15:27:15 +0900

    feat: 자주묻는질문 문구 변경 및 모듈화

--- app/landing/const.ts
+++ app/landing/const.ts
@@ -66,3 +66,88 @@ export const swipeBoxVariants = {
     },
   },
 };
+
+export const qnas = [
+  // {
+  //   idx: 0,
+  //   question: "넥스트룸은 무료 서비스인가요?",
+  //   answer: `누구나 힌트폰의 기본 기능을 무료로 사용할 수 있습니다.
+  //             <br />
+  //             다만 일부 기능은 유료로 제공하고 있어요.
+  //             <br />
+  //             <br />
+  //             현재 얼리버드 이벤트 기간으로, 49% 할인된 가격으로 제공하고
+  //             있습니다.
+  //             <br />
+  //             지금 구독하신 분들은 평생 동결 가격으로 넥스트룸을 사용하실 수
+  //             있어요!`,
+  // },
+  // {
+  //   idx: 1,
+  //   question: "구독 시 어떤 혜택이 있나요?",
+  //   answer: `손님들에게 보여줄 힌트에 사진을 첨부해 더 자세한 설명을 제공할 수
+  //             있어요.
+  //             <br />
+  //             타이머 화면의 배경을 테마 분위기에 맞게 커스텀하는 기능도
+  //             제공해요.
+  //             <br />더 자세한 내용은{" "}
+  //             <Link
+  //               href="https://www.notion.so/107febdc0ad180f09f68fc47e1f4fde2?pvs=21"
+  //               target="_blank"
+  //               rel="noopener noreferrer"
+  //             >
+  //               <span className="text-white">여기</span>
+  //             </Link>
+  //             에서 확인해 보세요.`,
+  // },
+  {
+    idx: 1,
+    question: "비용은 얼마인가요?",
+    answer: `현재 얼리버드 이벤트 기간으로, 비용은 월 29,900원입니다.
+지금 구독하신 분들은 평생 동결 가격으로 넥스트룸을 사용하실 수 있어요!
+결제는 앱에서 가능합니다.`,
+  },
+  {
+    idx: 2,
+    question:
+      "타이머 화면에서 게임을 종료하고 새로운 게임을 시작하는 방법이 궁금해요.",
+    answer: `타이머 화면의 좌측 상단에 희미하게 화살표가 있어요.
+화살표를 1초 이상 꾹 누르면 타이머 화면에서 나갈 수 있습니다.
+손님들이 실수로 게임을 종료하지 못하도록 설정해둔 소소한 장치에요.`,
+  },
+  {
+    idx: 3,
+    question: "비밀번호를 잊어버렸어요.",
+    answer: `현재 넥스트룸은 비밀번호 찾기/변경 기능을 제공하지 않습니다.
+만약 비밀번호를 잊으셨다면 공식계정(nextroom.official@gmail.com)으로 연락 주세요.`,
+  },
+  {
+    idx: 4,
+    question: "앱 사용중에 문제가 생겼어요.",
+    answer: `일시적으로 발생하는 문제가 아니라면 다음 연락망을 통해 제보해주세요.
+■ 공식 계정: nextroom.official@gmail.com
+<a href="https://www.instagram.com/team_nextroom/" target="_blank" rel="noopener noreferrer">■ 인스타그램: https://www.instagram.com/team_nextroom/</a>
+■ 연락처: 010-7416-9874
+<br />
+<img className="pc-faq-img" src="/images/landing/error-message.png" alt="error-message" width=240 />
+에러 팝업의 오류 내용 보기 버튼을 클릭하여 같이 제보해주시면
+더욱 빠른 조치가 가능합니다.`,
+  },
+  {
+    idx: 5,
+    question: "힌트 코드에는 숫자만 사용할 수 있나요?",
+    answer: `넥스트룸은 숫자만 지원하고 있습니다.
+손님들이 힌트 코드를 입력하는 데 시간을 지체하지 않기를 바라는 마음에 숫자만 지원하고 있어요.`,
+  },
+  {
+    idx: 6,
+    question: "안드로이드 폰에서만 사용 가능한가요?",
+    answer: `안드로이드 기기라면 폰, 태블릿 관계없이 사용 가능합니다. iOS는 지원하지 않습니다.`,
+  },
+  {
+    idx: 7,
+    question: "넥스트룸을 사용하기 위한 최소 사양을 알고 싶어요.",
+    answer: `최소 안드로이드 OS 버전이 7.0 이상이어야 다운로드 및 사용이 가능합니다.
+원활한 사용을 위해 램은 최소 3GB 이상을 권장드립니다.`,
+  },
+];
